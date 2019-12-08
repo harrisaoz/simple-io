6744e09212602439a09bbd5c491c8e9d7132ab9e
Anthony Harris <anthony@ah.id.au>
2019-12-08 19:59:35 +0000

    experimenting with isolating tests into other package.

--- src/main/scala/au/id/ah/sio/IO.scala
+++
@@ -1,6 +0,0 @@
-package au.id.ah.sio
-
-object IO {
-  def writeStringToFile(filename: FileName, text: String): Unit =
-    FileOverwriter.apply(filename).writeText(text)
-}
--- src/main/scala/au/id/ah/sio/package.scala
+++
@@ -1,8 +0,0 @@
-package au.id.ah
-
-package object sio {
-  type FolderName = String
-  type FileName = String
-  type WritableData[T] = T => Array[Byte]
-  type ByteWriterFactory = (FolderName, FileName) => Array[Byte] => Unit
-}
